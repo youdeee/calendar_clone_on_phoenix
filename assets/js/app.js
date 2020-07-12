5dfc058f1dc199767cc7dac7a543e71f7189f23e
youdeee <youdee.t.ud@gmail.com>
2020-07-12 14:39:44 +0900

    clean:

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,16 +13,3 @@ import '../css/app.scss'
 //     import socket from "./socket"
 //
 import 'phoenix_html'
-
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import App from './components/App'
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   ReactDOM.render(
-//     <React.StrictMode>
-//       <App />
-//     </React.StrictMode>,
-//     document.getElementById('root')
-//   )
-// })
